7146c78c06f452f529b7c69acde6d42222783926
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-05-17 18:48:13 +0900

    クリックでマスクを切り替える

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-document.onkeydown = function(e) {
+document.onkeydown = (e) => {
   if (e.keyCode === 32 || e.keyCode === 13) {
-    let num = Number(document.getElementById('mask').src.substr(-5, 1));
-    if (num == 9) {
-      document.getElementById('mask').src = `images/mask1.png`;
-    } else {
-      num++;
-      document.getElementById('mask').src = `images/mask${num}.png`;
-    }
+    changeMask();
   }
 };
+
+document.onclick = () => {
+  changeMask();
+};
+
+let changeMask = () => {
+  let num = Number(document.getElementById('mask').src.substr(-5, 1));
+  if (num == 9) {
+    document.getElementById('mask').src = `images/mask1.png`;
+  } else {
+    num++;
+    document.getElementById('mask').src = `images/mask${num}.png`;
+  }
+}
