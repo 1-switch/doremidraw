658973736092df65cddd015cc4502a080bad4aed
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-05-17 19:08:43 +0900

    切り替えに応じてドレミ音を出す

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 document.onkeydown = (e) => {
   if (e.keyCode === 32 || e.keyCode === 13) {
-    changeMask();
+    stepAction();
   }
 };
 
 document.onclick = () => {
-  changeMask();
+  stepAction();
 };
 
-let changeMask = () => {
+let stepAction = () => {
   let num = Number(document.getElementById('mask').src.substr(-5, 1));
   if (num == 9) {
     document.getElementById('mask').src = `images/mask1.png`;
   } else {
+    music = new Audio(`sounds/sound${num}.wav`);
+    music.play();
     num++;
     document.getElementById('mask').src = `images/mask${num}.png`;
   }
